6ba85c96f565261a97d81323c5dda74d87dc2f52
Juan <ssjuantj@gmail.com>
2021-08-17 22:52:07 -0300

    Part 2: Animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('Olá Juan');
+const btnHamburger = document.querySelector('#btnHamburger');
+const hamburger = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(hamburger.classList.contains('open')){ // Close Hamburger Menu
+        hamburger.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else { // Open Hamburger Menu
+        hamburger.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+})
