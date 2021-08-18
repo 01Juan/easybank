0f136e0f90c5d5116bc3ed8aaca90ae0f9ed485e
Juan <ssjuantj@gmail.com>
2021-08-18 09:58:55 -0300

    Part 3: Animated Mobile Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,26 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-const hamburger = document.querySelector('.header');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', function(){
     console.log('click hamburger');
 
-    if(hamburger.classList.contains('open')){ // Close Hamburger Menu
-        hamburger.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        body.classList.remove('noscroll');
+        header.classList.remove('open');
+        fadeElems.forEach( function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }
     else { // Open Hamburger Menu
-        hamburger.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        body.classList.add('noscroll');
+        header.classList.add('open');
+        fadeElems.forEach( function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        })
     }
 })
